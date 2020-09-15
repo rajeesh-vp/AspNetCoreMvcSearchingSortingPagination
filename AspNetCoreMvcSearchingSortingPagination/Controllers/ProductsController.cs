ae87c6d09127697875aef45914aba01b15143ccc
Rajeesh VP <rajeeshvp@gmail.com>
2020-09-15 11:58:45 +0530

    Search implemented

--- AspNetCoreMvcSearchingSortingPagination/Controllers/ProductsController.cs
+++ AspNetCoreMvcSearchingSortingPagination/Controllers/ProductsController.cs
@@ -13,87 +13,27 @@ namespace AspNetCoreMvcSearchingSortingPagination.Controllers
         public ProductsController()
         {
             ProductManager.SetProducts();
-            //ViewData["SortOrderParm"] = "desc";
         }
-        // GET: ProductsController
-        public ActionResult Index(string sortBy, string sortOrder)
+        public ActionResult Index(string sortBy, string sortOrder, string searchString)
         {
-            ViewData["sortOrderParm"] = sortOrder == null || sortOrder == "asc" ? "desc" : "asc";
+            var productLookup = ProductManager.GetProductLookup();
+            foreach (var item in productLookup)
+            {
+                if (item.Text == searchString)
+                    item.Selected = true;
+                else
+                    item.Selected = false;
+            }
+            ViewData["productLookup"] = productLookup;
 
+            ViewData["sortOrderParm"] = sortOrder == null || sortOrder == "asc" ? "desc" : "asc";
             string sortByArg = string.IsNullOrEmpty(sortBy) || sortBy == "name" ? "name" : "category";
             bool sortByAscending = string.IsNullOrEmpty(sortOrder) || sortOrder == "asc" ? true : false;
 
-            var products = ProductManager.GetProducts("", "", sortByArg, sortByAscending);
+            
+
+            var products = ProductManager.GetProducts(searchString, sortByArg, sortByAscending);
             return View(products);
-        }
-
-        // GET: ProductsController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: ProductsController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: ProductsController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ProductsController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: ProductsController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ProductsController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: ProductsController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
--- AspNetCoreMvcSearchingSortingPagination/Domain/ProductManager.cs
+++ AspNetCoreMvcSearchingSortingPagination/Domain/ProductManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,25 +12,39 @@ namespace AspNetCoreMvcSearchingSortingPagination.Domain
         public static void SetProducts()
         {
             _products = new List<Product>
-        {
-            new Product{ Id=new Guid(), Name ="Product1", Category = "Category3", Rate = 1000},
-            new Product{ Id=new Guid(), Name ="Product2", Category = "Category3", Rate = 2000},
-            new Product{ Id=new Guid(), Name ="Product3", Category = "Category3", Rate = 3000},
+            {
+                new Product{ Id=new Guid(), Name ="Product1", Category = "Category3", Rate = 1000},
+                new Product{ Id=new Guid(), Name ="Product2", Category = "Category3", Rate = 2000},
+                new Product{ Id=new Guid(), Name ="Product3", Category = "Category3", Rate = 3000},
 
-            new Product{ Id=new Guid(), Name ="Product4", Category = "Category2", Rate = 4000},
-            new Product{ Id=new Guid(), Name ="Product5", Category = "Category2", Rate = 5000},
-            new Product{ Id=new Guid(), Name ="Product6", Category = "Category2", Rate = 6000},
+                new Product{ Id=new Guid(), Name ="Product4", Category = "Category2", Rate = 4000},
+                new Product{ Id=new Guid(), Name ="Product5", Category = "Category2", Rate = 5000},
+                new Product{ Id=new Guid(), Name ="Product6", Category = "Category2", Rate = 6000},
 
-            new Product{ Id=new Guid(), Name ="Product7", Category = "Category1", Rate = 7000},
-            new Product{ Id=new Guid(), Name ="Product8", Category = "Category1", Rate = 8000},
-            new Product{ Id=new Guid(), Name ="Product9", Category = "Category1", Rate = 9000},
-        };
+                new Product{ Id=new Guid(), Name ="Product7", Category = "Category1", Rate = 7000},
+                new Product{ Id=new Guid(), Name ="Product8", Category = "Category1", Rate = 8000},
+                new Product{ Id=new Guid(), Name ="Product9", Category = "Category1", Rate = 9000},
+            };
         }
-        public static List<Product> GetProducts(string name, string category, string sortBy, bool sortByAscending)
+        public static List<SelectListItem> GetProductLookup()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem("Product1", "Product1"),
+                new SelectListItem("Product2", "Product2"),
+                new SelectListItem("Product3", "Product3"),
+                new SelectListItem("Product4", "Product4"),
+                new SelectListItem("Product5", "Product5"),
+                new SelectListItem("Product6", "Product6"),
+                new SelectListItem("Product7", "Product7"),
+                new SelectListItem("Product8", "Product8"),
+                new SelectListItem("Product9", "Product9"),
+            };
+        }
+        public static List<Product> GetProducts(string name, string sortBy, bool sortByAscending)
         {
             var query = _products
-                .Where(e => string.IsNullOrEmpty(name) ? true : e.Name == name)
-                .Where(e => string.IsNullOrEmpty(category) ? true : e.Category == category).AsQueryable();
+                .Where(e => string.IsNullOrEmpty(name) ? true : e.Name == name).AsQueryable();
 
             if (sortBy == "name")
             {
